c7b08c82d9109e77b60449791435ccdf77e647a4
Witold Majda <witoldmajda@wp.pl>
2018-10-20 19:52:28 +0200

    Rozbudowa Modelu

---
+++ WpfApp_20181013/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_20181013.Models
+{
+    public class BaseModel
+    {
+    }
+}
