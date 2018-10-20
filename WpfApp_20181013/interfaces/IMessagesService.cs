d5726d0797656bfa30272f139abbbb01675339f0
adminWM <adminWM@DESKTOP-DSGOPIN>
2018-10-20 19:26:41 +0200

    Rozbudowa 20181020

---
+++ WpfApp_20181013/interfaces/IMessagesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_20181013.interfaces
+{
+    public interface IMessagesService
+    {
+        ObservableCollection<object> Get();
+
+    }
+}
